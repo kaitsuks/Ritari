83ff1f7930aa7ec2a7351a602050b0acc696e13a
kaitsuks <37402060+kaitsuks@users.noreply.github.com>
2020-01-18 18:22:20 +0200

    GameManager
    Manager Prefab - GameManager script & SoundManager (to be used later), Loader in MainCamera.

---
+++ Assets/ReadSliderValue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadSliderValue : MonoBehaviour
+{
+    Slider slider;
+    Text tValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tValue = GetComponentInChildren<Text>();
+        slider = GameObject.Find("SliderTest").GetComponent<Slider>();
+    }
+
+    public void ReadIt()
+    {
+        tValue.text = slider.value.ToString(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/SetName.cs
+++ Assets/SetName.cs
@@ -7,17 +7,21 @@ public class SetName : MonoBehaviour
     string myName = "Your name is:";
     Text textName;
     InputField ifd;
+    GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         ifd = GetComponent<InputField>();
         textName = GameObject.Find("tPlayerName").GetComponent<Text>();
+        //gameManager = new GameManager(); //not allowed!!!
+        gameManager = GameObject.Find("Manager").GetComponent<GameManager>();
     }
 
     public void SetMyName()
     {
         textName.text = myName + ifd.text;
+        gameManager.player = textName.text;
     }
 
     // Update is called once per frame
